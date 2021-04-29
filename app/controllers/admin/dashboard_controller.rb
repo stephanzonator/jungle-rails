4ca73f4f5e91329c33a7a467c566930c4706c7f0
Stephanie Harrison <stephanzonator@hotmail.com>
2021-04-29 00:58:02 +0000

    admin dashboard now slightly more useful

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USER_ID"], password: ENV["PASSWORD"]
   def show
+    @products = Product.all
+    @categories = Category.all
   end
 end
