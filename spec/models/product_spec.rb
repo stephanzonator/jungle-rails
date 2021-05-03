59165e70fb6fca37c669828d8b1c076be067299c
Stephanie Harrison <stephanzonator@hotmail.com>
2021-05-03 22:31:00 +0000

    update all other tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -25,14 +25,60 @@ RSpec.describe Product, type: :model do
         @product = Product.new(:name => nil, :description => "test", :image => "/uploads/product/image/13/tiny_like.png", :price_cents => "200", :quantity => "1", :category_id => @category.id,)
         @product.save
 
+        # puts "***************************************************************"
+        # puts @product.errors.full_messages
+
         expect(@category).to be_present
         expect(@product).to be_present
-        expect(@product.name).to be_present
-        expect(@product.price_cents).to be_present
-        expect(@product.quantity).to be_present
-        expect(@product.category).to be_present
+        expect(@product.name).to be_nil
+        expect(@product.errors.full_messages).to include("Name can't be blank")
       end
     end
-   
+  
+    describe 'Price' do
+      it 'should correctly yield error if no product price present' do
+        @category = Category.new(:name => "Test categorybb")
+        @category.save
+        @product = Product.new(:name => "test name", :description => "test", :image => "/uploads/product/image/13/tiny_like.png", :price_cents => nil, :quantity => "1", :category_id => @category.id,)
+        @product.save
+
+        expect(@category).to be_present
+        expect(@product).to be_present
+        expect(@product.price).to be_nil
+        expect(@product.errors.full_messages).to include("Price can't be blank")
+      end
+    end
+
+    describe 'Quantity' do
+      it 'should correctly yield error if no product quantity present' do
+        @category = Category.new(:name => "Test categorybb")
+        @category.save
+        @product = Product.new(:name => "test name", :description => "test", :image => "/uploads/product/image/13/tiny_like.png", :price_cents => "200", :quantity => nil, :category_id => @category.id,)
+        @product.save
+
+        expect(@category).to be_present
+        expect(@product).to be_present
+        expect(@product.quantity).to be_nil
+        expect(@product.errors.full_messages).to include("Quantity can't be blank")
+      end
+    end
+
+    describe 'Category' do
+      it 'should correctly yield error if no product category present' do
+        @category = Category.new(:name => "Test categorybb")
+        @category.save
+        @product = Product.new(:name => nil, :description => "test", :image => "/uploads/product/image/13/tiny_like.png", :price_cents => "200", :quantity => "1", :category_id => nil,)
+        @product.save
+
+        # puts "***************************************************************"
+        # puts @product.errors.full_messages
+
+        expect(@category).to be_present
+        expect(@product).to be_present
+        expect(@product.category).to be_nil
+        expect(@product.errors.full_messages).to include("Category can't be blank")
+      end
+    end
+
   end
 end
