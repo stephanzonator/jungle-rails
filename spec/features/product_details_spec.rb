1bc583afe96245299cb47190d7ee03435fe3f42c
Stephanie Harrison <stephanzonator@hotmail.com>
2021-05-04 23:16:57 +0000

    test seems to be working correctly

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+    find('.product header').click
+
+    # visit '/products/1'
+
+    puts page.html
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to be_present
+    expect(page).to have_css '.product-detail'
+  end
+end
